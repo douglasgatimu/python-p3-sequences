283205f8f89cac2d93688b94034bd33b1672161a
Douglas Gatimu <douglasgatimu@gmail.com>
2025-07-12 06:30:37 +0300

    Added fibonacci.

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print([0, 1])
+    else:
+        fib = [0, 1]
+        n = 2
+        while n < length:
+            next = fib[-1] + fib[-2]
+            fib.append(next)
+            n += 1
+        print(fib)
+    
+
+
+
+# print_fibonacci(9)
